1e1a2182bcd51ac70a29442d489854fe701ad4ef
David <86719656+ScapularSteam@users.noreply.github.com>
2025-12-26 22:09:41 +0000

    practice fetching data from event table and structure as a dict

--- v1/testing/views.py
+++ v1/testing/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import event
 
 
 def index(request):
     return HttpResponse("Welcome, you're at the root of the Youthacks Portal")
+
+# Little test to structure event objects as a dict and sent as HTTP response
+def events_raw(request):
+    events_list_raw = event.objects.order_by("date")
+    events_list_parsed = []
+    for e in events_list_raw:
+        events_list_parsed.append(
+            dict({
+                "id": e.id,
+                "name": e.name,
+                "description": e.description,
+                "logo": e.logo,
+                "address": e.address,
+                "date": e.date
+            })
+        )
+    return HttpResponse(events_list_parsed)
